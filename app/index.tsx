15095dff4d37cd16193f9a0099373f9368ccf5ee
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2026-01-12 19:57:49 +0000

    ✨feat(app): Implemetacion de numero aleatorio -Se implementa logica para dado en index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import { useAccelerometer } from '../lib/modules/sensors/accelerometer/useAccelerometer';
+import { getDice } from '@/lib/core/logic/random_dice';
 
-export default function Index() {
+export default function App() {
+  const { shaking } = useAccelerometer();
+  const [number, setNumber] = useState<number | null>(null);
+
+  useEffect(() => {
+    if (shaking) {
+      // Cuando el hook avisa que se agita, la UI decide qué hacer
+      setNumber(getDice());
+    }
+  }, [shaking]); // Solo se ejecuta cuando cambia el estado de 'shaking'
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 40 }}>{number ?? '¡Agítame!'}</Text>
+      {shaking && <Text>Detectando movimiento...</Text>}
     </View>
   );
 }
---
+++ lib/core/logic/random_dice.ts
@@ -0,0 +1,3 @@
+export const getDice = (): number => {
+    return Math.floor(Math.random() * 12) + 1;
+  };
